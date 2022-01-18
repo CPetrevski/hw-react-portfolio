6c3b09407f4c046acdf06db7e5509f2b0245d871
Chris Petrevski <c_petrevski@hotmail.com>
2022-01-18 12:20:16 +1100

    home update

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import Typical from 'react-typical';
+import './Profile.css';
 
 export default function Profile() {
     return (
@@ -7,12 +8,14 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href='https://www.linkedin.com/in/chris-petrevski-44291b84' target="_blank" rel="noreferrer">
-                            <i className='fa fa-linkedin-square'></i>
-                        </a>
-                        <a href='https://github.com/CPetrevski' target="_blank" rel="noreferrer">
-                            <i className='fa fa-github-square'></i>
-                        </a>
+                        <div className='colz-icon'>
+                            <a href='https://www.linkedin.com/in/chris-petrevski-44291b84' target="_blank" rel="noreferrer">
+                                <i className='fa fa-linkedin-square'></i>
+                            </a>
+                            <a href='https://github.com/CPetrevski' target="_blank" rel="noreferrer">
+                                <i className='fa fa-github-square'></i>
+                            </a>
+                        </div>
                     </div>
                 
                     <div className='profile-details-name'>
@@ -37,6 +40,14 @@ export default function Profile() {
                             {""}
                             Contact Me{""}
                         </button>
+                        <a href="cp-resume.pdf" download="Chris Petrevski.pdf">
+                            <button className="btn highlighted-btn">Get Resume</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'>
+
                     </div>
                 </div>
             </div>
